bd4dc2a7924263261d2262a419a6a42a20bfe7f3
Jérémie Bolduc <jeremiebolducjs@gmail.com>
2024-12-01 14:06:08 -0500

    bayesian

--- Jeremie_Utils.jl
+++ Jeremie_Utils.jl
@@ -2,24 +2,8 @@ using GLM
 using MLJ
 using StatsModels
 using Turing
+using Distributions
 
-function remove_outliers(data)
-    model = lm(@formula(y ~ x), data) # to get encoding
-
-    X = modelmatrix(model)[:,2]
-    H = X * inv(X' * X) * X'
-    leverage = diag(H)
-
-    predictions =  StatsModels.predict(model)
-    residuals = y .- predictions
-
-    student_residuals = residuals ./ sqrt.(1 .- leverage)
-    treshold = 3
-    outliers = abs.(student_residuals) .> treshold
-    data = data[.!outliers, :]
-
-    return data
-end
 
 function remove_outliers(data, features::Vector{Symbol}, target::Symbol=:y)
     formula = Term(target) ~ sum(Term(Symbol(feature)) for feature in features)
@@ -42,41 +26,6 @@ function remove_outliers(data, features::Vector{Symbol}, target::Symbol=:y)
     return data
 end
 
-function mean_impute_outliers(data, features::Vector{Symbol}, target::Symbol=:y)
-    formula = Term(target) ~ sum(Term(Symbol(feature)) for feature in features)
-
-    model = lm(formula, data)
-
-    y = data[:, target]
-    X = modelmatrix(model)[:,2]
-    H = X * inv(X' * X) * X'
-    leverage = diag(H)
-
-    predictions = StatsModels.predict(model)
-    residuals = y .- predictions
-
-    student_residuals = residuals ./ sqrt.(1 .- leverage)
-    treshold = 3
-    outliers = abs.(student_residuals) .> treshold
-    for feature in features
-        if eltype(data[:, feature]) <: AbstractString || eltype(data[:, feature]) <: CategoricalValue
-            groups = groupby(data, feature)
-            for group in groups
-                mean_value = mean(group[:, target])
-
-                data[outliers .& (data[!, feature] .== group[1, feature]), target] .= mean_value
-            end
-        elseif eltype(data[:, feature]) <: AbstractFloat
-            df = DataFrame(x = data[!, feature], y = data[!, target])
-            mean_value = mean(df[.!outliers, :y])
-
-            data[outliers, target] .= mean_value
-        end
-    end
-
-    return data
-end
-
 function create_model(data, features::Vector{Symbol}, target::Symbol=:y)
     formula = Term(target) ~ sum(Term(feature) for feature in filter(x -> x != target, features))
 
@@ -93,16 +42,26 @@ function create_model(data, target::Symbol=:y)
     return model
 end
 
-function one_hot_encode(data, features)
-    for feature in features
-        if eltype(data[:, feature]) <: AbstractString
-            coerce!(data, feature => Multiclass)
-        end
+function encode(data, categorical_features, continuous_features, ordinal_features)
+    for feature in categorical_features
+        coerce!(data, feature => Multiclass)
     end
 
     mach = machine(OneHotEncoder(), data)
     fit!(mach, verbosity=0)
     data = MLJ.transform(mach, data)
+
+    for feature in continuous_features
+        coerce!(data, feature => MLJ.Continuous)
+    end
+
+    mach = machine(ContinuousEncoder(), data)
+    fit!(mach, verbosity=0)
+    data = MLJ.transform(mach, data)
+
+    for feature in ordinal_features
+        data[!, feature] = categorical(data[!, feature], ordered=true)
+    end
 
     return data
 end
@@ -131,10 +90,10 @@ function ridge_regression_cv(X, y, k_folds=5)
 
         cv_result = evaluate!(mach, resampling=cv, verbosity=0)
 
-        mean_score = mean(cv_result.measurement)
+        score = mean(cv_result.measurement)
 
-        if mean_score < best_score
-            best_score = mean_score
+        if score < best_score
+            best_score = score
             λ̂ = lambda
         end
     end
@@ -159,14 +118,15 @@ function lasso_regression_cv(X, y, k_folds=5)
 
         cv_result = evaluate!(mach, resampling=cv, verbosity=0)
 
-        mean_score = mean(cv_result.measurement)
+        score = mean(cv_result.measurement)
 
-        if mean_score < best_score
-            best_score = mean_score
+        if score < best_score
+            best_score = score
             λ̂ = lambda
         end
     end
 
+    println(λ̂)
     mach = machine(LassoRegressor(lambda=λ̂) , X, y)
     fit!(mach, verbosity=0)
 
@@ -176,8 +136,8 @@ end
 function elastic_net_regression_cv(X, y, k_folds=5)
     ElasticNetRegressor = @load ElasticNetRegressor pkg=MLJLinearModels verbosity=0
 
-    lambdas = 0.0:0.1:1.0
-    gammas = 0.0:0.1:1.0
+    lambdas = 10 .^ LinRange(-4, 0, 25)
+    gammas = 10 .^ LinRange(-4, 0, 25)
 
     cv = CV(nfolds=k_folds)
 
@@ -191,10 +151,10 @@ function elastic_net_regression_cv(X, y, k_folds=5)
             mach = machine(model, X, y)
             cv_result = evaluate!(mach, resampling=cv, verbosity=0)
 
-            mean_score = mean(cv_result.measurement)
+            score = mean(cv_result.measurement)
 
-            if mean_score < best_score
-                best_score = mean_score
+            if score < best_score
+                best_score = score
                 λ̂ = lambda
                 γ̂ = gamma
             end
@@ -204,34 +164,42 @@ function elastic_net_regression_cv(X, y, k_folds=5)
     mach = machine(ElasticNetRegressor(lambda=λ̂, gamma=γ̂), X, y)
     fit!(mach, verbosity=0)
 
+    # return λ̂, γ̂
     return mach
 end
 
 @model function bayesian_regression(X, y)
+    features = names(X)
+    n = size(X, 1)
     predictors = size(X, 2)
-    α ~ Normal(0, 10)
 
-    β ~ filldist(TDist(3), predictors)
-    # β ~ MvNormal(predictors, 10)
-    # β = zeros(predictors)
-    # for i in 1:predictors
-    #     β[i] ~ Laplace(0, 1)
-    # end
+    β = zeros(predictors)
+    for i in 1:predictors
+        if features[i] == "cylindree"
+            β[i] ~ LogNormal(5.981630883156576, 3.6476067440594417)
+        elseif features[i] == "nombre_cylindres"
+            β[i] ~ Normal(5.479391634980988, 1.7817038897119009)
+        else 
+            β[i] ~ Laplace(0, 1)
+        end
+    end
+    α ~ Normal(mean(y), std(y))
+
     σ² ~ InverseGamma(2, 1)
 
-    μ = α .+ X * β
-    Σ = sqrt(σ²) * I
+    μ = α .+ Matrix(X) * β
+    Σ = σ² * I
 
     return y ~ MvNormal(μ, Σ)
 end
 
 # https://turinglang.org/docs/tutorials/05-linear-regression/
 function bayesian_prediction(chain, X)
-    params = get_params(chain[200:end, :, :])
+    params = get_params(chain[50:end, :, :])
 
     α̂ = params.α
     β̂ = reduce(hcat, params.β)
-    ŷ_dists = α̂' .+ X * β̂'
+    ŷ_dists = α̂' .+ Matrix(X) * β̂'
 
     return [mean(ŷ_dists[i, :]) for i in 1:size(ŷ_dists, 1)]
 end

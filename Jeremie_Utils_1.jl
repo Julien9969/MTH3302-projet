d311847490060c43ce532e3a657f9846a49b7eea
Julien9969 <julien.roux404@gmail.com>
2024-12-04 02:05:09 -0500

    comment

---
+++ Jeremie_Utils_1.jl
@@ -0,0 +1,213 @@
+using GLM
+using MLJ
+using StatsModels
+using Turing
+
+function remove_outliers(data, features::Vector{Symbol}, target::Symbol=:y)
+    formula = Term(target) ~ sum(Term(Symbol(feature)) for feature in features)
+
+    model = lm(formula, data)
+
+    y = data[:, target]
+    X = modelmatrix(model)[:,2]
+    H = X * inv(X' * X) * X'
+    leverage = diag(H)
+
+    predictions =  StatsModels.predict(model)
+    residuals = y .- predictions
+
+    student_residuals = residuals ./ sqrt.(1 .- leverage)
+    treshold = 3
+    outliers = abs.(student_residuals) .> treshold
+    data = data[.!outliers, :]
+
+    return data
+end
+
+function linear_model(data, features::Vector{Symbol}, target::Symbol=:y)
+    formula = Term(target) ~ sum(Term(feature) for feature in filter(x -> x != target, features))
+
+    model = lm(formula, data)
+
+    return model
+end
+
+function create_model(data, target::Symbol=:y)
+    formula = Term(target) ~ sum(Term(Symbol(feature)) for feature in filter(x -> Symbol(x) != target, names(data)))
+
+    model = lm(formula, data)
+
+    return model
+end
+
+function encode(data, nominal_features, continuous_features, ordinal_features)
+    for feature in nominal_features
+        coerce!(data, feature => Multiclass)
+    end
+
+    mach = machine(OneHotEncoder(), data)
+    fit!(mach, verbosity=0)
+    data = MLJ.transform(mach, data)
+
+    for feature in continuous_features
+        coerce!(data, feature => MLJ.Continuous)
+    end
+
+    mach = machine(ContinuousEncoder(), data)
+    fit!(mach, verbosity=0)
+    data = MLJ.transform(mach, data)
+
+    for feature in ordinal_features
+        data[!, feature] = categorical(data[!, feature], ordered=true)
+    end
+
+    return data
+end
+
+function get_updated_features(data, features) 
+    return vec(reduce(vcat, [
+        filter(name -> startswith(name, string(feature)), names(data))
+        for feature in features
+    ]))
+end
+
+function linear_regression(X, y)
+    LinearRegressor = @load LinearRegressor pkg=MLJLinearModels verbosity=0 
+    linear_regressor = LinearRegressor()
+
+    mach = machine(linear_regressor, X, y)
+    fit!(mach, verbosity=0)
+
+    return mach
+end
+
+RidgeRegressor = @load RidgeRegressor pkg=MLJLinearModels verbosity=0 
+function ridge_regression_cv(X, y, k_folds=5)
+    lambdas = 10 .^ LinRange(-4, 4, 100)
+    cv = CV(nfolds=k_folds)
+
+    best_score = Inf
+    λ̂ = nothing
+
+    for lambda in lambdas
+        model = RidgeRegressor(lambda=lambda)
+        mach = machine(model, X, y)
+
+        cv_result = evaluate!(mach, resampling=cv, verbosity=0)
+
+        score = mean(cv_result.measurement)
+
+        if score < best_score
+            best_score = score
+            λ̂ = lambda
+        end
+    end
+
+    mach = machine(RidgeRegressor(lambda=λ̂) , X, y)
+    fit!(mach, verbosity=0)
+
+    return mach, λ̂
+end
+
+LassoRegressor = @load LassoRegressor pkg=MLJLinearModels verbosity=0 
+function lasso_regression_cv(X, y, k_folds=5)
+    lambdas = 10 .^ LinRange(-4, 4, 100)
+    cv = CV(nfolds=k_folds)
+
+    best_score = Inf
+    λ̂ = nothing
+
+    for lambda in lambdas
+        model = LassoRegressor(lambda=lambda)
+        mach = machine(model, X, y)
+
+        cv_result = evaluate!(mach, resampling=cv, verbosity=0)
+
+        score = mean(cv_result.measurement)
+
+        if score < best_score
+            best_score = score
+            λ̂ = lambda
+        end
+    end
+
+    mach = machine(LassoRegressor(lambda=λ̂) , X, y)
+    fit!(mach, verbosity=0)
+
+    return mach
+end
+
+function elastic_net_regression_cv(X, y, k_folds=5)
+    ElasticNetRegressor = @load ElasticNetRegressor pkg=MLJLinearModels verbosity=0
+
+    lambdas = 10 .^ LinRange(-4, 0, 25)
+    gammas = 10 .^ LinRange(-4, 0, 25)
+
+    cv = CV(nfolds=k_folds)
+
+    best_score = Inf
+    λ̂ = nothing
+    γ̂ = nothing
+
+    for lambda in lambdas
+        for gamma in gammas
+            model = ElasticNetRegressor(lambda=lambda, gamma=gamma)
+            mach = machine(model, X, y)
+            cv_result = evaluate!(mach, resampling=cv, verbosity=0)
+
+            score = mean(cv_result.measurement)
+
+            if score < best_score
+                best_score = score
+                λ̂ = lambda
+                γ̂ = gamma
+            end
+        end
+    end
+
+    mach = machine(ElasticNetRegressor(lambda=λ̂, gamma=γ̂), X, y)
+    fit!(mach, verbosity=0)
+
+    return mach
+end
+
+@model function bayesian_regression(X, y, priors)
+    features = names(X)
+    predictors = size(X, 2)
+
+    α ~ Normal(mean(y), std(y))
+    β = zeros(predictors)
+    for i in 1:predictors
+        if haskey(priors, features[i])
+            β[i] ~ priors[features[i]]
+        else 
+            β[i] ~ Normal(0, 3)
+        end
+    end
+    if (haskey(priors, "σ²"))
+        σ² ~ priors["σ²"]
+    else 
+        σ² ~ InverseGamma(1, 2)
+    end
+
+    μ = α .+ Matrix(X) * β
+    Σ = σ² * I
+
+    return y ~ MvNormal(μ, Σ)
+end
+
+# https://turinglang.org/docs/tutorials/05-linear-regression/
+function bayesian_prediction(chain, X)
+    params = get_params(chain)
+
+    α̂ = params.α
+    β̂ = reduce(hcat, params.β)
+    targets = α̂' .+ Matrix(X) * β̂'
+
+    return vec(mean(targets; dims=2))
+end
+
+function standardize_vec(x)
+    centered = x .- mean(x)
+    return centered ./ std(x)
+end
--- Julien_Test_LinearRegressor.jl
+++ Julien_Test_LinearRegressor.jl
@@ -3,14 +3,18 @@ using DataFrames
 using MLJ
 using Random
 using Statistics
+using MLJDecisionTreeInterface
 
-Random.seed!(1234)
+Random.seed!(4355)
 
 train_data = CSV.read("train.csv", DataFrame, decimal=',')
 test_data = CSV.read("test.csv", DataFrame, decimal=',')
 
+# consommation au carre
+# train_data[!,:consommation] = train_data[:, :consommation] .^ 2
 
-function remove_outliers(data, column)
+
+function remove_outliers(data, column, alpha=1.5)
     unique_values = unique(data[:, column])
     for value in unique_values
         subset = data[data[:, column] .== value, :]
@@ -20,8 +24,8 @@ function remove_outliers(data, column)
         q1 = quantile(subset[:, :consommation], 0.25)
         q3 = quantile(subset[:, :consommation], 0.75)
         iqr = q3 - q1
-        lower_bound = q1 - 1.5 * iqr
-        upper_bound = q3 + 1.5 * iqr
+        lower_bound = q1 - alpha * iqr
+        upper_bound = q3 + alpha * iqr
         data = data[(data[:, column] .!= value) .| ((data[:, column] .== value) .& (data[:, :consommation] .>= lower_bound) .& (data[:, :consommation] .<= upper_bound)), :]
     end
     return data
@@ -29,17 +33,29 @@ end
 
 filtered_train = copy(train_data)
 
-filtered_train = remove_outliers(filtered_train, :annee);
-filtered_train = remove_outliers(filtered_train, :type);
-filtered_train = remove_outliers(filtered_train, :nombre_cylindres);
-filtered_train = remove_outliers(filtered_train, :transmission);
-filtered_train = remove_outliers(filtered_train, :boite);
+# best=0.4 0.5641931723004245
+
+println("Before removing outliers: ", size(train_data, 1))
+
+alpha = 1.5
+filtered_train = remove_outliers(filtered_train, :annee, alpha);
+filtered_train = remove_outliers(filtered_train, :type, alpha);
+filtered_train = remove_outliers(filtered_train, :nombre_cylindres, alpha);
+filtered_train = remove_outliers(filtered_train, :transmission, alpha);
+filtered_train = remove_outliers(filtered_train, :boite, alpha);
+println("After removing outliers: ", size(filtered_train, 1))
+
+
+schema(filtered_train)
 
 # describe(filtered_train[:, :consommation])
 
-# filtered_train[:, :annee] = Float64.(filtered_train[:, :annee])
 
+# remove annee
+# filtered_train = filtered_train[:, Not(:boite)]
+# test_data = test_data[:, Not(:boite)]
 
+# filtered_train[!, :annee] = Float64.(filtered_train[:, :annee])
 categorical_cols = [:type, :transmission, :boite, :annee]
 
 function one_hot_encode(data, columns)
@@ -58,38 +74,101 @@ end
 
 filtered_train = one_hot_encode(filtered_train, categorical_cols)
 filtered_train[!,:nombre_cylindres] = Float64.(filtered_train[:, :nombre_cylindres])
+
+# filtered_train[!,:annee] = Float64.(filtered_train[:, :annee])
 # println(schema(filtered_train))
 # println(eltype(filtered_train[:, :nombre_cylindres]))
+
 
 
 X = select(filtered_train, Not(:consommation))
 y = filtered_train[:, :consommation]          
 
-train, test = partition(eachindex(y), 0.8, shuffle=true) 
+train, valid = partition(eachindex(y), 0.8, shuffle=true) 
 X_train, y_train = X[train, :], y[train]
-X_test, y_test = X[test, :], y[test]
+X_valid, y_valid = X[valid, :], y[valid]
+
+
+# https://juliaai.github.io/MLJ.jl/dev/model_stacking/
+DecisionTreeRegressor = @load DecisionTreeRegressor pkg=DecisionTree verbosity=0
+EvoTreeRegressor = @load EvoTreeRegressor pkg=EvoTrees verbosity=0
+XGBoostRegressor = @load XGBoostRegressor pkg=XGBoost verbosity=0
+KNNRegressor = @load KNNRegressor pkg=NearestNeighborModels verbosity=0
+LinearRegressor = @load LinearRegressor pkg=MLJLinearModels verbosity=0
+
+stack = Stack(;metalearner=LinearRegressor(),
+                resampling=CV(),
+                measures=rmse,
+                constant=ConstantRegressor(),
+                tree_2=DecisionTreeRegressor(max_depth=2),
+                tree_3=DecisionTreeRegressor(max_depth=3),
+                evo=EvoTreeRegressor(),
+                knn=KNNRegressor(),
+                xgb=XGBoostRegressor()
+                )
+
+mach = machine(stack, X_train, y_train)
+
+fit!(mach, verbosity=0)
+
+y_pred = collect(predict(mach, X_valid))
+
+println("Stack RMSE: ", rms(y_pred, y_valid))
+
 
 
 model = @load LinearRegressor pkg=MLJLinearModels
-machine_ = machine(model(), X_train, y_train)
-fit!(machine_)
+linear_machine = machine(model(), X_train, y_train)
 
-# y_pred = predict_mode(machine_, X_test)
-y_pred = y_pred = collect(predict(machine_, X_test))
+tree_model = @load RandomForestRegressor pkg=DecisionTree
+forest = machine(tree_model(n_trees=100), X_train, y_train)
+
+fit!(linear_machine)
+fit!(forest)
+
+y_pred_linear = collect(predict(linear_machine, X_valid))
+y_pred_forest = collect(predict(forest, X_valid))
+
+println("Linear RMSE: ", rms(y_pred_linear, y_valid))
+println("Random Forest RMSE: ", rms(y_pred_forest, y_valid))
 
 
-# evaluator = evaluate!(machine_, resampling=CV(shuffle=true), measure=[rms], verbosity=0)
-# println("RMSE : ", evaluator.measurement)
-
-println("RMSE : ", rms(y_pred, y_test))
-# println("values pred : ", y_pred)
+#####################
+# Test data prediction
+#####################
 
 X_test = one_hot_encode(test_data, categorical_cols)
 
-machine_ = machine(model(), X, y)
-fit!(machine_)
+# linear_machine = machine(model(), X, y)
+# forest = machine(tree_model(n_trees=100), X, y)
+# fit!(forest)
 
-y_pred = predict_mode(machine_, X_test)
+# y_pred = predict_mode(forest, X_test)
+
+
+DecisionTreeRegressor = @load DecisionTreeRegressor pkg=DecisionTree verbosity=0
+EvoTreeRegressor = @load EvoTreeRegressor verbosity=0
+XGBoostRegressor = @load XGBoostRegressor verbosity=0
+KNNRegressor = @load KNNRegressor pkg=NearestNeighborModels verbosity=0
+LinearRegressor = @load LinearRegressor pkg=MLJLinearModels verbosity=0
+
+stack = Stack(;metalearner=LinearRegressor(),
+                resampling=CV(),
+                measures=rmse,
+                constant=ConstantRegressor(),
+                tree_2=DecisionTreeRegressor(max_depth=2),
+                tree_3=DecisionTreeRegressor(max_depth=3),
+                evo=EvoTreeRegressor(),
+                knn=KNNRegressor(),
+                xgb=XGBoostRegressor())
+
+mach = machine(stack, X, y)
+
+evaluate!(mach; resampling=Holdout(), measure=rmse)
+println(report(mach).cv_report)
+fit!(mach, verbosity=0)
+
+y_pred = collect(predict(mach, X_valid))
 
 n = size(y_pred, 1)
 id = 1:n
